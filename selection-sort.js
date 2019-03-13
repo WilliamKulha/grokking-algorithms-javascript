e831308e92cc70a2d4466f452fd65278387b5732
WilliamKulha <wkulha14@gmail.com>
2019-03-13 08:42:24 -0400

    Remove bug that kept selection sort from going through the entire array. Remove comments and console statements.

--- selection-sort.js
+++ selection-sort.js
@@ -1,26 +1,23 @@
-//For Numbers
 let testingList = [15, 5, 12, 9, 13, 1, 14, 2, 11, 6, 4, 3, 8, 10, 7]
 
-//Function to find the smallest number
 const findSmallest = (arr) => {
+  let length = arr.length;
   let smallest = arr[0];
   let smallestIndex = 0;
 
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < length; i++) {
     if (arr[i] < smallest) {
       smallest = arr[i];
       smallestIndex = i;
     }
   }
-  console.log(smallestIndex);
   return smallestIndex;
-
 }
 
-
 const selectionSort = (arr) => {
-  newArr = [];
-  for (let i = 0; i < arr.length; i++) {
+  let length = arr.length;
+  let newArr = [];
+  for (let i = 0; i < length; i++) {
     let smallest = findSmallest(arr);
     newArr.push(arr[smallest]);
     arr.splice(smallest, 1);
