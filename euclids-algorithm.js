14eb37e23b1067d6d90f20377d15c4060814e7f6
WilliamKulha <wkulha14@gmail.com>
2019-03-13 21:52:10 -0400

    Fix bug by adding return statement to recursive case. Remove console log.

--- euclids-algorithm.js
+++ euclids-algorithm.js
@@ -1,10 +1,9 @@
 const gcd = (smallNumber, bigNumber) => {
   
   if (bigNumber % smallNumber === 0) {
-    console.log(smallNumber);
     return smallNumber
   }
   else {
-    gcd((bigNumber % smallNumber), smallNumber);
+    return gcd((bigNumber % smallNumber), smallNumber);
   }
 }
