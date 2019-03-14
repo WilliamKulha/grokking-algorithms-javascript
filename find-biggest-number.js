6370e2dcc09d298ca0ae8741367afe96b299fba9
WilliamKulha <wkulha14@gmail.com>
2019-03-14 06:49:04 -0400

    Add recursive function to find the largest number in an array.

---
+++ find-biggest-number.js
@@ -0,0 +1,19 @@
+const findBiggestNumber = (arr) => {
+  
+  if (arr.length === 2) {
+    if (arr[0] > arr[1]) {
+      return arr[0];
+    } else {
+      return arr[1];
+    }
+  } else {
+    if (arr[0] < arr[1]) {
+      arr.shift();
+      return findBiggestNumber(arr);
+    } else {
+      arr.splice(1, 1);
+      return findBiggestNumber(arr);
+    }
+  
+  }       
+}
